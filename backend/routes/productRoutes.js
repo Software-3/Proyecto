bd4dfbf97a8ecdd5be807dfa6f9b1ba51832d154
Juan Ortiz <feltri08@gmail.com>
2022-02-12 16:27:13 -0500

    Todos los css agregados
    Agregados todos los css - todos los .js en limpio, solo el file

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {
-    getProducts,
-    getProductById,
-  } = require("../controller/productControllers");
+  getProducts,
+  getProductById,
+} = require("../controller/productControllers");
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <div className="app">Hello World</div>;
+
+  return <div className = "app">Hello World</div>;
 }
 
 export default App;
