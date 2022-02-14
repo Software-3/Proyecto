6c6c20f45419a906e1453a7d706acc5007a1f244
Juan Ortiz <feltri08@gmail.com>
2022-02-13 22:06:47 -0500

    App terminada v1

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,38 +1,42 @@
 import './Navbar.css';
 import {Link} from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 const Navbar = ({click}) => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
+
   return (
     <nav className="navbar">
-        {/* logo */}
         <div className="navbar__logo">
             <h2>TIENDA DE COMERCIOS</h2>
         </div>
     
-        {/* links */}
         <ul className="navbar__links">
           <li>
               <Link to='/cart' className="cart__link">
                 <i className="fas fa-shopping-cart"></i>
-                Cart
-                <span className="cartlogo__badge">0</span>
+                <span>
+                  Cart <span className="cartlogo__badge">{getCartCount()}</span>
+                </span>
               </Link>
           </li>
           <li>
-              <Link to='/'>
-                Shop
-              </Link>
+              <Link to='/'>Shop</Link>
           </li>
         </ul>
     
-        {/* Hamburguer menu */}
         <div className="hamburger__menu" onClick={click}>
           <div></div>
           <div></div>
           <div></div>
         </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
